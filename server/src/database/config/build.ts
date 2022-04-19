d112eecbe681cff479260cb4b839634bb61ebfc4
WessalJawad95 <wessaljawad95@gmail.com>
2022-04-20 02:50:00 +0300

    setup data relates #5

--- server/src/database/config/build.ts
+++ server/src/database/config/build.ts
@@ -1,1 +1,10 @@
-export {};
+import { readFileSync } from 'fs';
+import { join } from 'path';
+import connection from './connection';
+
+const dbBuild = () => {
+  const sql = readFileSync(join(__dirname, 'build.sql')).toString();
+  return connection.query(sql);
+};
+
+export default dbBuild;
