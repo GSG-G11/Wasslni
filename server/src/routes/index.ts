4a73cecbb1914a853536e37cbd8f79e7d2254bc7
unknown <montherismail90@gmail.com>
2022-04-20 20:40:19 +0300

    create twilio functionality Relates #13

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,9 +1,12 @@
 import { Router } from 'express';
+import { sendSMS, verifySMS } from '../utils/twilio';
 
 const router = Router();
 
 router.get('/', (req, res) => {
   res.send('Hello World');
 });
+router.post('/signup', sendSMS);
+router.post('/verifySMS', verifySMS);
 
 export default router;
