f447f350cad03c5852bc4107b77d7ca4ae91035d
Fady-Alwazir <fady.alwazir@gmail.com>
2022-04-21 18:10:38 +0300

    refactoring checkAuth middlware Relates #31

--- server/src/middleware/checkAuth.ts
+++ server/src/middleware/checkAuth.ts
@@ -1,22 +1,17 @@
 import * as dotenv from 'dotenv';
-import { CustomError, verifyToken } from '../utils';
+import { verifyToken } from '../utils';
 
 dotenv.config();
 
 const checkAuth = async (req: any, res: any, next: any) => {
-  try {
-    const { token } = req.cookies;
-    if (!token) {
-      throw new CustomError('You are not authorized', 401);
-    }
-    const decoded:any = await verifyToken(token, process.env.PRIVATE_KEY);
-    const { id } = decoded;
-    req.userId = id;
-
-    next();
-  } catch (error) {
-    next(error);
+  const { token } = req.cookies;
+  if (!token) {
+    return res.status(401).json({ message: 'Unauthorized' });
   }
+  const decoded: any = await verifyToken(token, process.env.PRIVATE_KEY);
+  const { id } = decoded;
+  req.userId = id;
+  return next();
 };
 
 export default checkAuth;
