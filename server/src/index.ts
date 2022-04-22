2d9e0fcc36d8827065ef1e763371101763a5a7ad
mayar-kabaja <kabajamayar@gmail.com>
2022-04-23 02:17:09 +0300

    delete console.log and comments relates #57

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,8 +2,4 @@ import app from './app';
 
 const port = app.get('port');
 
-// fetch(
-//   'https://api.mapbox.com/directions/v5/mapbox/driving/34.45%2C31.52%3B31.50%2C34.42?alternatives=true&geometries=geojson&language=en&overview=simplified&steps=true&access_token=pk.eyJ1IjoiZmFkeWFsd2F6aXIiLCJhIjoiY2wyM2Q1aWtmMW92MjNibHBoMDUwam9udiJ9.zSFwxv09NAf6m6o2vbgC_g',
-// )
-//   .then((res) => res.json()).then((res) => console.log(res));
 app.listen(port, () => console.log(`Running on http://localhost:${port}`));
