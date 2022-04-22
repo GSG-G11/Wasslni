e6d4fc8591ec4c71f5fe82dd73a09c6ee4d73b2d
WessalJawad95 <wessaljawad95@gmail.com>
2022-04-22 16:32:10 +0300

    update getParcels

--- server/src/controllers/parcels/getParcels.ts
+++ server/src/controllers/parcels/getParcels.ts
@@ -4,7 +4,7 @@ const getParcels = async (req:any, res:any) => {
   const { userId } = req;
 
   const { rows, rowCount } = await getParcelsQuery(userId);
-  if (rowCount === 0) {
+  if (!rowCount) {
     return res.status(404).json({ message: 'no parcels found' });
   }
   return res.status(200).json({ message: 'parcels uploaded', data: rows[0] });
