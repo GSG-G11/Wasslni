c584ea8eb6f5bf663aa3a7debe20e952b6e22782
Fady-Alwazir <fady.alwazir@gmail.com>
2022-04-23 14:31:58 +0300

    solving conflict in userQueries index Relates #61

--- server/src/database/queries/userQuieries/index.ts
+++ server/src/database/queries/userQuieries/index.ts
@@ -1,15 +1,9 @@
 import signUpDB from './signUpDB';
 import checkPhoneNumber from './checkPhoneNumber';
 import getUserById from './getUserById';
-<<<<<<< HEAD
 import editPasswordDB from './editPasswordDB';
-
-export {
-  signUpDB, checkPhoneNumber, getUserById, editPasswordDB,
-=======
 import editProfileQuery from './editProfile';
 
 export {
-  signUpDB, checkPhoneNumber, getUserById, editProfileQuery,
->>>>>>> 83217684f0dbc3d7087fc4809a64a6224031f555
+  signUpDB, checkPhoneNumber, getUserById, editPasswordDB, editProfileQuery,
 };
