e495a861e70a88523777002a0867c870f1b8fdde
Fady-Alwazir <fady.alwazir@gmail.com>
2022-04-23 21:37:40 +0300

    checking profile route Relates #68

--- server/__test__/routes.test.ts
+++ server/__test__/routes.test.ts
@@ -1,20 +1,28 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import request from 'supertest';
 import {
-  afterAll, beforeAll, test,
+  afterAll, beforeAll, describe, test,
 } from '@jest/globals';
+
 import app from '../src/app';
 import connection from '../src/database/config/connection';
 import dbBuild from '../src/database/config/build';
 
 beforeAll(() => dbBuild());
-test('', (done) => {
-  request(app)
-    .get('/api/v1')
-    .expect(200)
-    .end((err) => {
-      if (err) return done(err);
-      return done();
-    });
+
+describe('testing profile routes', () => {
+  test(' test get api/v1/profile should return a 401 status code if there is no token', (done) => {
+    request(app)
+      .get('/api/v1/profile')
+      .expect(401)
+      .expect('Content-Type', /json/)
+      .end((err) => {
+        if (err) {
+          return done(err);
+        }
+        return done();
+      });
+  });
 });
+
 afterAll(() => connection.end());
