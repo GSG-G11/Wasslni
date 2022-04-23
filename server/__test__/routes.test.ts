0cf5e4a0932abf9d7077ed0adc610262eab111d9
WessalJawad95 <wessaljawad95@gmail.com>
2022-04-24 01:33:34 +0300

    test route /api/v1/parcels relates #68

--- server/__test__/routes.test.ts
+++ server/__test__/routes.test.ts
@@ -1,20 +1,24 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import request from 'supertest';
 import {
-  afterAll, beforeAll, test,
+  afterAll, beforeAll, test, describe,
 } from '@jest/globals';
 import app from '../src/app';
 import connection from '../src/database/config/connection';
 import dbBuild from '../src/database/config/build';
 
 beforeAll(() => dbBuild());
-test('', (done) => {
-  request(app)
-    .get('/api/v1')
-    .expect(200)
-    .end((err) => {
-      if (err) return done(err);
-      return done();
-    });
+
+describe('check route "api/v1/parcels" ', () => {
+  test('should return 401 when user Unauthorized', (done) => {
+    request(app)
+      .get('/api/v1/parcels')
+      .expect(401)
+      .end((err) => {
+        if (err) return done(err);
+        return done();
+      });
+  });
 });
+
 afterAll(() => connection.end());
