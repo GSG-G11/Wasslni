bd9198eb6dc2f169898e13764691b57cc730a19a
mayar-kabaja <kabajamayar@gmail.com>
2022-04-23 23:45:42 +0300

    test sms route

--- server/__test__/routes.test.ts
+++ server/__test__/routes.test.ts
@@ -9,6 +9,44 @@ import connection from '../src/database/config/connection';
 import dbBuild from '../src/database/config/build';
 
 beforeAll(() => dbBuild());
+describe('check route "api/v1/sms"  ', () => {
+  test('should return 200 when twilio send a message to the owner of the phoneNumber  ', (done) => {
+    request(app)
+      .post('/api/v1/sms')
+      .send({
+        phoneNumber: '+972567359920',
+      })
+      .expect(200)
+      .end((err) => {
+        if (err) return done(err);
+        return done();
+      });
+  });
+  test('should return 400 ( Bad request ) When the user enters a number less than and more than 13 digits ', (done) => {
+    request(app)
+      .post('/api/v1/sms')
+      .send({
+        phoneNumber: '+97256735992',
+      })
+      .expect(400)
+      .end((err) => {
+        if (err) return done(err);
+        return done();
+      });
+  });
+  test('should return 400 ( Bad request ) When user sends unexpected data type ', (done) => {
+    request(app)
+      .post('/api/v1/sms')
+      .send({
+        phoneNumber: 4,
+      })
+      .expect(400)
+      .end((err) => {
+        if (err) return done(err);
+        return done();
+      });
+  });
+});
 
 describe('testing profile routes', () => {
   test(' test get api/v1/profile should return a 401 status code if there is no token', (done) => {
@@ -16,10 +54,48 @@ describe('testing profile routes', () => {
       .get('/api/v1/profile')
       .expect(401)
       .expect('Content-Type', /json/)
+      // eslint-disable-next-line consistent-return
       .end((err) => {
         if (err) {
           return done(err);
         }
+      });
+  });
+});
+describe('check route "api/v1/sms"  ', () => {
+  test('should return 200 when twilio send a message to the owner of the phoneNumber  ', (done) => {
+    request(app)
+      .post('/api/v1/sms')
+      .send({
+        phoneNumber: '+972567359920',
+      })
+      .expect(200)
+      .end((err) => {
+        if (err) return done(err);
+        return done();
+      });
+  });
+  test('should return 400 ( Bad request ) When the user enters a number less than and more than 13 digits ', (done) => {
+    request(app)
+      .post('/api/v1/sms')
+      .send({
+        phoneNumber: '+97256735992',
+      })
+      .expect(400)
+      .end((err) => {
+        if (err) return done(err);
+        return done();
+      });
+  });
+  test('should return 400 ( Bad request ) When user sends unexpected data type ', (done) => {
+    request(app)
+      .post('/api/v1/sms')
+      .send({
+        phoneNumber: 4,
+      })
+      .expect(400)
+      .end((err) => {
+        if (err) return done(err);
         return done();
       });
   });
