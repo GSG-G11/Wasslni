2910c6adfe39b891818042e799ad42241099f2ad
Fady-Alwazir <fady.alwazir@gmail.com>
2022-04-25 22:54:11 +0300

    set up custom boostrap Relates #77

--- client/src/index.js
+++ client/src/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import './scss/custom.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
-
