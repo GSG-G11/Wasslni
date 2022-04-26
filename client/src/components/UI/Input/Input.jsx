55c1bafa85d6a2bc31fec32392bd5079fb5de698
unknown <montherismail90@gmail.com>
2022-04-26 17:26:41 +0300

    create base64 functionality Relates #92

--- client/src/components/UI/Input/Input.jsx
+++ client/src/components/UI/Input/Input.jsx
@@ -6,12 +6,21 @@ import PropTypes from 'prop-types';
 import TextError from '../../Form/ErrorMsg/ErrorMsg';
 
 function Input({
-  name, type, placeholder, ...props
+  name, type, placeholder, onChange, handleChange, ...props
 }) {
   return (
     <div className=" m-4">
 
-      <Field id={name} className="form-control" name={name} type={type} placeholder={placeholder} {...props} />
+      <Field
+        id={name}
+        className="form-control"
+        name={name}
+        type={type}
+        placeholder={placeholder}
+        onchange={onChange}
+        handleChange={handleChange}
+        {...props}
+      />
 
       <ErrorMessage name={name} component={TextError} className="error-message" />
     </div>
@@ -23,6 +32,7 @@ Input.propTypes = {
   name: PropTypes.string.isRequired,
   type: PropTypes.string.isRequired,
   placeholder: PropTypes.string.isRequired,
-
+  onChange: PropTypes.func.isRequired,
+  handleChange: PropTypes.func.isRequired,
 };
 export default Input;
--- client/src/utils/index.js
+++ client/src/utils/index.js
@@ -1,3 +1,6 @@
+import getBase64Image from './getBase64';
 import { signupValidation, smsValidation, loginValidation } from './validation';
 
-export { signupValidation, smsValidation, loginValidation };
+export {
+  signupValidation, smsValidation, loginValidation, getBase64Image,
+};
