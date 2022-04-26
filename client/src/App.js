93ebf52a2d88272174d2af46c62425f5e6498409
mayar-kabaja <kabajamayar@gmail.com>
2022-04-26 20:52:12 +0300

    install react-router-dom & fix in hom page Relates 95#

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import { Home } from './pages';
 
 function App() {
   return (
-    <Home />
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
   );
 }
 
