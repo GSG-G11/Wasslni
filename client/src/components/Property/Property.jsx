90a18720cf720508e0e710d929e9fcabe1ef15b1
WessalJawad95 <wessaljawad95@gmail.com>
2022-04-26 23:22:06 +0300

    create property component relates #98

---
+++ client/src/components/Property/Property.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import propTypes from 'prop-types';
+import './Property.css';
+
+function Property({ keyWord, value }) {
+  return (
+    <div className="clientContainer">
+      <span className="keyWord">
+        {keyWord}
+        :
+      </span>
+      <span className="value">{value}</span>
+    </div>
+  );
+}
+
+Property.propTypes = {
+  keyWord: propTypes.string.isRequired,
+  value: propTypes.string.isRequired,
+};
+
+export default Property;
