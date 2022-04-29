f8b3ab407d6eef88f3a005484da738d00e403659
Fady-Alwazir <fady.alwazir@gmail.com>
2022-04-29 22:27:43 +0300

    fix phone number validation Relates #107

--- client/src/utils/validation/userValidation/loginValidation.js
+++ client/src/utils/validation/userValidation/loginValidation.js
@@ -1,8 +1,8 @@
 import * as Yup from 'yup';
 
 const loginValidation = Yup.object().shape({
-  phoneNumber: Yup.string()
-    .required('Required'),
+  phoneNumber: Yup.string().min(9, 'الرقم يجب ان يتكون من 9 أرقام').max(9, 'الرقم يجب ان يتكون من 9 أرقام').matches(/^[0-9]*$/, 'الرقم يجب ان يتكون من 9 أرقام')
+    .required(' هذه الخانة مطلوبة'),
   password: Yup.string()
     .required('Required'),
 });
--- client/src/utils/validation/userValidation/smsValidation.js
+++ client/src/utils/validation/userValidation/smsValidation.js
@@ -1,6 +1,7 @@
 import * as Yup from 'yup';
 
 const smsValidation = Yup.object().shape({
-  phoneNumber: Yup.string().min(9, 'الرقم يجب ان يتكون من 9 أرقام').max(9, 'الرقم يجب ان يتكون من 9 أرقام').required('هذه الخانة مطلوبة '),
+  phoneNumber: Yup.string().min(9, 'الرقم يجب ان يتكون من 9 أرقام').max(9, 'الرقم يجب ان يتكون من 9 أرقام').matches(/^[0-9]*$/, 'الرقم يجب ان يتكون من 9 أرقام')
+    .required(' هذه الخانة مطلوبة'),
 });
 export default smsValidation;
