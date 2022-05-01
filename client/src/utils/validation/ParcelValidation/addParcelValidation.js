b31f1507329bbad1f73733446c2698a0b2e971ad
mayar-kabaja <kabajamayar@gmail.com>
2022-05-01 16:49:01 +0300

    fix schema Relates #115

--- client/src/utils/validation/ParcelValidation/addParcelValidation.js
+++ client/src/utils/validation/ParcelValidation/addParcelValidation.js
@@ -2,11 +2,11 @@ import * as Yup from 'yup';
 
 const addParcelSchema = Yup.object().shape({
   name: Yup.string()
-    .required('Required'),
+    .required(' هذه الخانة مطلوبة'),
   phoneNumber: Yup.string().min(9, 'الرقم يجب ان يتكون من 9 أرقام').max(9, 'الرقم يجب ان يتكون من 9 أرقام').matches(/^[0-9]*$/, 'الرقم يجب ان يتكون من 9 أرقام')
     .required(' هذه الخانة مطلوبة'),
   price: Yup.string()
-    .required('Required'),
+    .required(' هذه الخانة مطلوبة'),
 });
 
 export default addParcelSchema;
