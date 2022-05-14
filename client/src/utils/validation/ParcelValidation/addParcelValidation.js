332c88f44081f965fa26f0e045601d2344ea4604
Fady-Alwazir <fady.alwazir@gmail.com>
2022-05-14 18:09:34 +0300

    edit parcel validation schema Relates #186

--- client/src/utils/validation/ParcelValidation/addParcelValidation.js
+++ client/src/utils/validation/ParcelValidation/addParcelValidation.js
@@ -5,7 +5,7 @@ const addParcelSchema = Yup.object().shape({
     .required(' هذه الخانة مطلوبة'),
   phoneNumber: Yup.string().min(9, 'الرقم يجب ان يتكون من 9 أرقام').max(9, 'الرقم يجب ان يتكون من 9 أرقام').matches(/^[0-9]*$/, 'الرقم يجب ان يتكون من 9 أرقام')
     .required(' هذه الخانة مطلوبة'),
-  price: Yup.string()
+  price: Yup.string().matches(/^[0-9]*$/, 'يجب ادخال رقم')
     .required(' هذه الخانة مطلوبة'),
 });
 
