e6a1b26c64c61837d58a5b06c509d07d0c5e5aef
Fady-Alwazir <fady.alwazir@gmail.com>
2022-05-14 16:27:53 +0300

    set up sentry

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,13 +2,32 @@ import express from 'express';
 import * as dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 import { join } from 'path';
+import * as Sentry from '@sentry/node';
+import * as Tracing from '@sentry/tracing';
 import { clientError, serverError } from './controllers';
 import router from './routes';
 
 dotenv.config();
-
+const { env: { PORT, NODE_ENV, SENTRY_DSN } } = process;
 const app = express();
-const { env: { PORT, NODE_ENV } } = process;
+
+Sentry.init({
+  dsn: SENTRY_DSN,
+  integrations: [
+    // enable HTTP calls tracing
+    new Sentry.Integrations.Http({ tracing: true }),
+    // enable Express.js middleware tracing
+    new Tracing.Integrations.Express({ app }),
+  ],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
+app.use(Sentry.Handlers.requestHandler());
+// TracingHandler creates a trace for every incoming request
+app.use(Sentry.Handlers.tracingHandler());
 
 app.set('port', PORT || 5000);
 app.use([express.json(), express.urlencoded({ extended: true }), cookieParser()]);
@@ -22,6 +41,7 @@ if (NODE_ENV === 'production') {
   });
 }
 app.use(clientError);
+app.use(Sentry.Handlers.errorHandler());
 app.use(serverError);
 
 export default app;
